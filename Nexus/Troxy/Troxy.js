e01f55a8ea8a489f9aa1070279e41f6bb76f10c7
Marcus Gosselin <marcus13345@live.com>
2017-05-24 16:23:47 -0400

    fixed troxy a bit

--- Nexus/Troxy/Troxy.js
+++ Nexus/Troxy/Troxy.js
@@ -140,7 +140,7 @@
 			});
 
 			sock.on('connect', function () {
-				this.Vlt.started = true;
+				that.Vlt.started = true;
 				console.log('Proxy - Connected on host:' + host + ', port:' + port);
 				Vlt.Sock = sock;
 				fun();
@@ -200,7 +200,7 @@
 
 
 	function Start(com, fun) {
-		if (!(this.Par.Start) && ("Setup" in this.Par)){
+		if (!(this.Par.Start) && ("Setup" in this.Par) && !this.Vlt.started){
 			fun();
 			return;
 		}
@@ -369,7 +369,7 @@
 			});
 
 			sock.on('connect', function () {
-				this.Vlt.started = true;
+				that.Vlt.started = true;
 				console.log('Troxy - Connected on host:' + host + ', port:' + port);
 				Vlt.Sock = sock;
 				fun();
