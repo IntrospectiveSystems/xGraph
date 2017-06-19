9947d8234bd9990a5e45c568370d0f6fbfa9236d
Marcus Gosselin <marcus13345@live.com>
2017-06-19 13:39:25 -0400

    xfer layout initializer and layout manager

---
+++ Widgets/LayoutInitializer/LayoutInitializer.js
@@ -0,0 +1,63 @@
+//# sourceURL=LayoutInitializer.js
+//jshint esversion: 6
+(function LayoutInitializer() {
+
+	class LayoutInitializer {
+		Setup(com, fun) {
+			// debugger;
+			fun(null, com);
+		}
+		Start(com, fun) {
+			let that = this;
+			// debugger;
+			function parseView(view, fun) {
+				if (typeof view == 'string') {
+					console.log('PID ' + view);
+					fun(view);
+				} else {
+					let basePid = view.View;
+					async.each(view.Children, function(item, next) {
+						parseView(item, (pid) => {
+							that.send({
+								Cmd: "AddView",
+								View: pid
+							}, basePid, () => {
+								next();
+							});
+						});
+					}, function() {
+						//done adding children
+						console.log('PID ' + basePid);
+						console.log('DAT WAY');
+						fun(basePid);
+					});
+				}
+			}
+
+			parseView(this.Par.Layout, (apexPid) => {
+				// debugger;
+				this.send({ Cmd: "GetViewRoot" }, apexPid, (err, com) => {
+					// debugger;
+					$(document.body).append(com.Div);
+
+					this.send({ Cmd: "ShowHierarchy" }, apexPid, () => { });
+
+					$(window).resize(() => {
+						this.send({ Cmd: "Resize" }, apexPid, () => { });
+					});
+
+					this.send({ Cmd: "DOMLoaded" }, apexPid, (err, com) => {
+						fun(null, com);
+					});
+				});
+			});
+		}
+	}
+
+	return {
+		dispatch: LayoutInitializer.prototype
+	};
+
+})();
+
+
