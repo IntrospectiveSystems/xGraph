fea4595db47242fa63ebde25b83d3c932fab3cec
Trevor Gionet <tjgionet@gmail.com>
2017-10-19 08:53:23 -0400

    update path for mac

--- Nexus/Bin/xGraph.js
+++ Nexus/Bin/xGraph.js
@@ -3,7 +3,7 @@ console.log("Executing");
 console.log(process.cwd());
 process.env.NODE_PATH = "node_modules/";
 const { spawn } = require('child_process');
-const ls = spawn("node", ["/home/trevor/xGraph/Nexus/Nexus/Nexus.js", ...process.argv], { env: process.env });
+const ls = spawn("node", ["/Users/tjgionet/xGraph/Nexus/Nexus/Nexus.js", ...process.argv], { env: process.env });
 
 ls.stdout.on('data', (data) => {
 	console.log(`stdout: ${data}`);
