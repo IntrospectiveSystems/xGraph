8e70a5a6aa94b20579e72383fd778cb45973a44c
Trevor Gionet <tjgionet@gmail.com>
2017-10-19 09:04:22 -0400

    mac binary xMac

--- Nexus/Bin/xGraph.js
+++ Nexus/Bin/xGraph.js
@@ -1,16 +1,20 @@
 console.log("Executing");
 
 console.log(process.cwd());
+console.log(JSON.stringify(process.env, null, 2));
+
 process.env.NODE_PATH = "node_modules/";
 const { spawn } = require('child_process');
-const ls = spawn("node", ["/Users/tjgionet/xGraph/Nexus/Nexus/Nexus.js", ...process.argv], { env: process.env });
+
+console.log(`\nNexus Path: ${process.env.HOME}/xGraph/Nexus/Nexus/Nexus.js`);
+const ls = spawn("node", [process.env.HOME+"/xGraph/Nexus/Nexus/Nexus.js", ...process.argv], { env: process.env });
 
 ls.stdout.on('data', (data) => {
-	console.log(`stdout: ${data}`);
+	console.log(`${data}`);
 });
 
 ls.stderr.on('data', (data) => {
-	console.log(`stderr: ${data}`);
+	console.log(`${data}`);
 });
 
 ls.on('close', (code) => {
