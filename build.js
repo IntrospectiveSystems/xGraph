60b34c2dab48ac88baed289c4f17359ac37a0061
Trevor Gionet <tjgionet@gmail.com>
2017-10-20 17:05:32 -0400

    build code

--- build.js
+++ build.js
@@ -8,7 +8,7 @@
 	
 	await compile({
 		input: 'src/gen/xgraph-linux.js',
-		output: 'bin/linux/xgraph',
+		output: 'bin/linux/bin/xgraph',
 		target: 'linux-x64-8.4.0',
 		bundle: true
 	});
@@ -17,7 +17,7 @@
 	
 	await compile({
 		input: 'src/gen/xgraph-windows.js',
-		output: 'bin/windows/xgraph',
+		output: 'bin/windows/bin/xgraph',
 		target: 'windows-x64-8.4.0',
 		bundle: true
 	});
@@ -26,9 +26,22 @@
 	
 	await compile({
 		input: 'src/gen/xgraph-mac.js',
-		output: 'bin/mac/xgraph',
+		output: 'bin/mac/bin/xgraph',
 		target: 'mac-x64-8.4.0',
 		bundle: true
 	});
+
+	//move all required files to lib of system bin.
+	try {fs.mkdirSync('bin/lib');}catch(e){console.log(e);}
+	try {fs.mkdirSync('bin/lib/Nexus');}catch(e){console.log(e);}
+	try {fs.writeFileSync('bin/lib/Nexus/Nexus.js',fs.readFileSync('Nexus/Nexus/Nexus.js'));}catch(e){console.log(e);}
+	try {fs.writeFileSync('bin/lib/Nexus/package.json',fs.readFileSync('Nexus/Nexus/package.json'));}catch(e){console.log(e);}
+	try {fs.mkdirSync('bin/lib/Proxy');	}catch(e){console.log(e);}
+	try {fs.writeFileSync('bin/lib/Proxy/Proxy.js',fs.readFileSync('Nexus/Proxy/Proxy.js'));}catch(e){console.log(e);}
+	try {fs.writeFileSync('bin/lib/Proxy/schema.json',fs.readFileSync('Nexus/Proxy/schema.json'));}catch(e){console.log(e);}
+
+	
+	//make the tar.gz ... msi ... mac dmg/pkg
+
 
 })();
--- src/xgraph.js
+++ src/xgraph.js
@@ -1,4 +1,4 @@
-const {execSync} = require('child_process');
+const { execSync } = require('child_process');
 const tar = require('targz');
 const fs = require('fs');
 const mergedirs = require('merge-dirs').default;
@@ -28,33 +28,27 @@ let unix = false;
 let cwd = (process.cwd());
 let bindir = process.argv[0].substr(0, process.argv[0].lastIndexOf('/'));
 
-// console.log('OS:', system);
+if (process.argv.length == 1) process.argv[1] = 'run';
 
-// console.log(...process.argv);
-
-if(process.argv.length == 1) process.argv[1] = 'run';
-
-switch(process.argv[1]) {
-  case 'run': {
-    run();
-    break;
-  }
-  case 'help':
-  case '--help': {
-    help();
-    break;
-  }
-  default: {
-    console.log(`unknown command <${process.argv[1]}>`);
-    help();
-    break;
-  }
+switch (process.argv[1]) {
+	case 'run': {
+		run();
+		break;
+	}
+	case 'help':
+	case '--help': {
+		help();
+		break;
+	}
+	default: {
+		console.log(`unknown command <${process.argv[1]}>`);
+		help();
+		break;
+	}
 }
 
-
-
 function help() {
-  console.log(`
+	console.log(`
   xGraph
   Introspective Systems LLC
 
@@ -68,88 +62,101 @@ function help() {
 }
 
 async function run() {
-  try{
-    await new Promise((resolve, reject) => ensureNode(_ => {if(_) resolve(); else reject();}));
-    console.log('look for config/cache here: ' + cwd);
-    console.log('executable is here: ' + bindir);
-  }catch(e) {
-    console.log(`ERR: ${e}`);
-  }
+	try {
+		await new Promise((resolve, reject) => ensureNode(_ => { if (_) resolve(); else reject(); }));
+		console.log('look for config/cache here: ' + cwd);
+		console.log('executable is here: ' + bindir);
+		startChildProcess();
+	} catch (e) {
+		console.log(`ERR: ${e}`);
+	}
 }
 
 function ensureNode(fun) {
-  if(linux) {
-    let node = (execSync('which node').toString());
-    node = '';
-    if(node != '') {
-      console.log();
-      fun(true);
-    }
-    else {
-      require('https').get({
-        host: 'nodejs.org',
-        path: '/dist/v8.4.0/node-v8.4.0-linux-x64.tar.gz'
-      }, (response) => {
-        let body = '';
-        response.pipe(fs.createWriteStream(bindir + '/node.tar.gz'));
-        response.on('end', function() {
-          // console.log('extraction time!');
-          tar.decompress({
-            src: bindir + '/node.tar.gz',
-            dest: bindir
-          }, function() {
-            // console.log(mergedirs);
-            try {
-              mergedirs('node-v8.4.0-linux-x64/bin', '/usr/bin', 'overwrite');
-              mergedirs('node-v8.4.0-linux-x64/include', '/usr/include', 'overwrite');
-              mergedirs('node-v8.4.0-linux-x64/lib', '/usr/lib', 'overwrite');
-              mergedirs('node-v8.4.0-linux-x64/share', '/usr/share', 'overwrite');
-              //TODO RIMRAF THE ZIP AND EXTRACTED FILES
-              console.log('dun');
-            }catch(e) {
-              console.log('Could not install node, try running the command again with sudo\n');
-              console.log("If the problem persists, email support@introspectivesystems.com");
-              console.log('with this ' + e.toString());
-            }
-            
-          });
-        });
-      });
-    }
-  }
+	// #ifdef LINUX
+	let node = (execSync('which node').toString());
+
+	if (node != '') {
+		console.log();
+		fun(true);
+	}
+	else {
+		install();
+	}
+	// #else
+	console.error(`System ${system} is not yet supported`);
+	// #endif
 }
 
+function install() {
+	// #ifdef LINUX
 
-//set all command line arguments to ENV variables
-// let arg;
-// for (let iarg = 0; iarg < process.argv.length; iarg++) {
-// 	arg = process.argv[iarg];
-// 	console.log(arg);
-// 	parts = arg.split('=');
-// 	if (parts.length == 2) {
-// 		if (parts[0].toLowerCase() == "xgraph"){
-// 			process.env['XGRAPH'] = parts[1];
-// 		}
-// 		else{
-// 			process.env[parts[0]] = parts[1];
-// 		}
-// 	}
-// }
+	require('https').get({
+		host: 'nodejs.org',
+		path: '/dist/v8.4.0/node-v8.4.0-linux-x64.tar.gz'
+	}, (response) => {
+		let body = '';
+		response.pipe(fs.createWriteStream(bindir + '/node.tar.gz'));
+		response.on('end', function () {
+			// console.log('extraction time!');
+			tar.decompress({
+				src: bindir + '/node.tar.gz',
+				dest: bindir
+			}, function () {
+				// console.log(mergedirs);
+				try {
+					mergedirs('node-v8.4.0-linux-x64/bin', '/usr/bin', 'overwrite');
+					mergedirs('node-v8.4.0-linux-x64/include', '/usr/include', 'overwrite');
+					mergedirs('node-v8.4.0-linux-x64/lib', '/usr/lib', 'overwrite');
+					mergedirs('node-v8.4.0-linux-x64/share', '/usr/share', 'overwrite');
+					//TODO RIMRAF THE ZIP AND EXTRACTED FILES
+					console.log('dun');
+				} catch (e) {
+					console.log('Could not install node, try running the command again with sudo\n');
+					console.log("If the problem persists, email support@introspectivesystems.com");
+					console.log('with this ' + e.toString());
+				}
+			});
+		});
+	});
+	// #else
+	console.error(`System ${system} is not yet supported`);	
+	// #endif
+}
 
-// process.env.NODE_PATH = "node_modules/";
-// const { spawn } = require('child_process');
+function startChildProcess() {
 
-// console.log(`\nNexus Path: ${process.env.XGRAPH}/Nexus/Nexus/Nexus.js`);
-// const ls = spawn("node", [process.env.XGRAPH+"/Nexus/Nexus/Nexus.js", ...process.argv], { env: process.env });
+	// set all command line arguments to ENV variables
+	let arg;
+	for (let iarg = 0; iarg < process.argv.length; iarg++) {
+		arg = process.argv[iarg];
+		console.log(arg);
+		parts = arg.split('=');
+		if (parts.length == 2) {
+			if (parts[0].toLowerCase() == "xgraph") {
+				process.env['XGRAPH'] = parts[1];
+			}
+			else {
+				process.env[parts[0]] = parts[1];
+			}
+		}
+	}
 
-// ls.stdout.on('data', (data) => {
-// 	console.log(`${data}`);
-// });
+	process.env.NODE_PATH = "node_modules/";
+	const { spawn } = require('child_process');
 
-// ls.stderr.on('data', (data) => {
-// 	console.log(`${data}`);
-// });
+	console.log(`\nNexus Path: ${bindir.substr(0, bindir.lastIndexOf('/'))}/lib/Nexus/Nexus.js`);
+	const ls = spawn("node", [process.env.XGRAPH + "/Nexus/Nexus/Nexus.js", ...process.argv], { env: process.env });
 
-// ls.on('close', (code) => {
-// 	console.log(`child process exited with code ${code}`);
-// });
+	ls.stdout.on('data', (data) => {
+		console.log(`${data}`);
+	});
+
+	ls.stderr.on('data', (data) => {
+		console.log(`${data}`);
+	});
+
+	ls.on('close', (code) => {
+		console.log(`child process exited with code ${code}`);
+	});
+}
