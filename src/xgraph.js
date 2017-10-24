b662ed8f32cc4981d465b71c2cf3720230ac3eff
Marcus Gosselin <marcus@mandworks.com>
2017-10-24 12:53:12 -0400

    switches have meaning!!

--- src/xgraph.js
+++ src/xgraph.js
@@ -25,10 +25,17 @@ let mac = false;
 let unix = false;
 // #endif
 
+let pathOverrides = {};
+
 let cwd = (process.cwd());
 let bindir = process.argv[0].substr(0, process.argv[0].lastIndexOf('/'));
 
+let configFile = null; // purposefully null
+let cacheDir = null;
+
 if(process.argv.length == 1) process.argv[1] = 'run';
+
+processSwitches();
 
 switch(process.argv[1]) {
   case 'run': {
@@ -53,7 +60,7 @@ switch(process.argv[1]) {
 }
 
 async function init(args) {
-  
+  console.log('init System');
   console.log('[', ...args, ']');
 
 }
@@ -74,7 +81,7 @@ function help() {
 
 async function run() {
 	try {
-		await new Promise((resolve, reject) => ensureNode(_ => { if (_) resolve(); else reject(); }));
+		await ensureNode();
 		console.log('look for config/cache here: ' + cwd);
 		console.log('executable is here: ' + bindir);
 		startChildProcess();
@@ -83,16 +90,15 @@ async function run() {
 	}
 }
 
-function ensureNode(fun) {
+async function ensureNode() {
 	// #ifdef LINUX
 	let node = (execSync('which node').toString());
 
 	if (node != '') {
 		console.log();
 		fun(true);
-	}
-	else {
-		install();
+	} else {
+		await install();
 	}
 	// #else
 	console.error(`System ${system} is not yet supported`);
@@ -100,7 +106,6 @@ function ensureNode(fun) {
 }
 
 function install() {
-
 	return new Promise((resolve) => {
     // #ifdef LINUX
     require('https').get({
@@ -122,7 +127,7 @@ function install() {
             mergedirs('node-v8.4.0-linux-x64/lib', '/usr/lib', 'overwrite');
             mergedirs('node-v8.4.0-linux-x64/share', '/usr/share', 'overwrite');
             //TODO RIMRAF THE ZIP AND EXTRACTED FILES
-            console.log('dun');
+            // console.log('dun');
             resolve();
           }catch(e) {
             console.log('Could not install node, try running the command again with sudo\n');
@@ -138,6 +143,35 @@ function install() {
     console.error(`System ${system} is not yet supported`);
     // #endif
   });
+}
+
+function processSwitches() {
+  for(let i = 0; i < process.argv.length; i ++) {
+    let str = process.argv[i];
+    if(str.startsWith('--')) {
+      let key = process.argv[i].slice(2);
+      applySwitch(key, i);
+    }
+  }
+}
+
+function applySwitch(str, i) {
+  let val = null;
+  if ((i+1) in process.argv) { // switch has a value
+    val = process.argv[i+1];
+  }
+  switch(key) {
+    case 'config': {
+      configFile = val;
+      break;
+    }
+    case 'cache': {
+      cacheDir = val;
+    }
+    default: {
+      pathOverrides[key] = val;
+    }
+  }
 }
 
 //set all command line arguments to ENV variables
