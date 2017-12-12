8809c8af49abd6f5c40022881670f8b65ff98c02
Trevor Gionet <tjgionet@gmail.com>
2017-12-12 14:16:17 -0500

    Updates to xor

--- XOR/XOR.js
+++ XOR/XOR.js
@@ -1,63 +1,79 @@
 //# sourceURL=XOR.js
 (function XOR() {
 
-
 	//-----------------------------------------------------dispatch
 	let dispatch = {
 		Start: Start,
-		BeginLoop:BeginLoop,
-		Utility:Utility
+		BeginLoop: BeginLoop,
+		Utility: Utility
 	};
 
 	return {
 		dispatch: dispatch
 	};
 
-	function Start(com, fun){
-		console.log("--XOR/Start");
+	function Start(com, fun) {
+		log.v("--XOR/Start");
 		let that = this;
 		let Vlt = this.Vlt;
 		let Par = this.Par;
 		Vlt.Iteration = 0;
-		Vlt.function  = (function (x,y){
+		Vlt.function = (function (x, y) {
 			if (x == y)
 				return [0];
 			else return [1];
 		});
-		setTimeout(()=>{this.send({Cmd:"BeginLoop"},Par.Pid)},1500);
+		setTimeout(() => { this.send({ Cmd: "BeginLoop" }, Par.Pid) }, 1000);
 		fun(null, com);
 	}
 
-
-	function BeginLoop(com, fun){
-		console.log("--XOR/BeginLoop");
-		let Par= this.Par;
-		let Vlt= this.Vlt;
-		let that=this;
-		let q={Cmd:"Evaluate"};
+	function BeginLoop(com, fun) {
+		log.v("--XOR/BeginLoop");
+		let Par = this.Par;
+		let Vlt = this.Vlt;
+		let that = this;
+		let q = { Cmd: "Train" };
+		let r = { Cmd: "Evaluate" };
 
 		Iterate();
 
-		function Iterate(){
+		function Iterate() {
 			// determine a new state of two random values, 0 and 1
-			q.State = [Math.round(Math.random()), Math.round(Math.random())];
-			console.log("\nState", q.State);
-			that.send(q, Par.Brain, (err,cm) => {
-				console.log(Vlt.Iteration, " Returned: Action ",cm.Action);
+			q.Input = [Math.round(Math.random()), Math.round(Math.random())];
+			q.OutPut = Vlt.function(...q.Input);
+			log.v("\nInput", q.Input, "\nOutput", q.Output);
+			that.send(q, Par.Brain, (err, cm) => {
+				log.v(Vlt.Iteration, " Returned: Action ", cm.Action);
 				Vlt.Iteration++;
-				if (Vlt.Iteration <300){
-					setTimeout(Iterate,100);
+				if (Vlt.Iteration < 300) {
+					setTimeout(Iterate, 100);
+				} else {
+					Evaluate();
 				}
 			});
 		}
-		
+
+		function Evaluate() {
+			q.Input = [Math.round(Math.random()), Math.round(Math.random())];
+			log.v("\nInput", q.Input);
+			that.send(q, Par.Brain, (err, cm) => {
+				log.v(Vlt.Iteration, " Returned: Output ", cm.Output);
+				Vlt.Iteration++;
+				if (Vlt.iteration < 350) {
+					setTimeout(Evaluate, 100);
+				}else{
+					log.v("Done!!");
+				}
+			});
+		}
+
 		if (fun)
 			fun(null, com);
 	}
 
-	function Utility(com, fun){
-		//console.log("--XOR/Utility", com.State, com.Action);
-		com.Output = (1- Math.abs(this.Vlt.function(...com.State)[0] - com.Action));
+	function Utility(com, fun) {
+		//log.v("--XOR/Utility", com.State, com.Action);
+		com.Output = (1 - Math.abs(this.Vlt.function(...com.State)[0] - com.Action));
 		fun(null, com);
 	}
 
