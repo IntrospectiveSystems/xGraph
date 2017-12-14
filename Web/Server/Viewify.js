c4109c03db90b030abea39f838c1c023a9a54e5b
Marcus Gosselin <marcus@mandworks.com>
2017-12-14 13:06:37 -0500

    idek

--- Web/Server/Viewify.js
+++ Web/Server/Viewify.js
@@ -306,10 +306,10 @@ if (!window.Viewify) window.Viewify = function Viewify(_class, versionString) {
 			this.Vlt.root.append(this.Vlt.div);
 			fun(null, com);
 		}
-		
-		/**
-		 * @description deprecated
-		 * Set the color of the View
+				/**
+   		 * @description Set the color of the View
+		 * 
+		 * @deprecated
 		 * @param {any} com 
 		 * @param {any} fun 
 		 * @memberof View
