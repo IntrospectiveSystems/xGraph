474a72bb9255c4f8fdf3eea57078410c516291e3
unknown <amanda.castonguay@introspectivesystems.com>
2018-01-16 16:15:58 -0500

    Working on CSV module.

---
+++ Extensions/Modules/CSV/CSV.js
@@ -0,0 +1,19 @@
+//# sourceURL=CSV.js
+(function CSV() {
+	class CSV {
+		Setup(com, fun) {
+			//this function is typically used to allow the entity/module to handle any internal setup
+			//procedures prior to being connected to by other entities/modules
+
+			fun(null, com);
+		}
+
+		Start(com, fun){
+			//this function is typically used to allow the entity/module to handle any external setup
+			//procedures
+
+			fun(null, com);
+		}
+	}
+	return {dispatch:CSV.prototype};
+})();
