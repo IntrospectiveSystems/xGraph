516b398456ffb4a93171c7181bd30e94627ba64d
Marcus Gosselin <marcus13345@live.com>
2018-03-20 17:40:55 -0400

    change module not found error message to be more clear

--- Core/src/Genesis.js
+++ Core/src/Genesis.js
@@ -647,13 +647,13 @@
 						try {
 							files = fs.readdirSync(containingPath);
 						} catch (err) {
-							err += ' Module <' + containingPath + '? not available'
+							err += ' \nModule \"' + containingPath + '\" not available'
 							log.e(err);
 							fun(err);
 							return;
 						}
 						if (!files) {
-							err += ' Module <' + containingPath + '? not available'
+							err += ' \nModule \"' + containingPath + '\" not available'
 							log.e(err);
 							fun(err);
 							return;
