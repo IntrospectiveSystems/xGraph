06cedf0c3201a59c019c9c4fce203be10509c4af
Trevor Gionet <tjgionet@gmail.com>
2018-03-23 10:07:12 -0400

    building the bandit

--- Extensions/Modules/MultiArmedBandit/Bandit.js
+++ Extensions/Modules/MultiArmedBandit/Bandit.js
@@ -1,19 +1,5 @@
 //# sourceURL=Bandit.js
 (function Bandit() {
-	//a general purpose multi-dimensional searching algorithm that interrogates
-	//the dispatching function
-	// com.Data = {
-	//  State : [] //the current action that was chosen
-	// 	Min: [],
-	// 	Max: [],
-	// 	StepSize: [], //percent
-	// 	Iterations: 5, //number
-	// 	Delta: 0.03 //percent
-	// };
-	//
-	// com.Host= pid; //id of a host of value function
-	// com.EvalName= "Evaluate";
-	// com.Mode= "Max"; //or "Min"
 
 	//-----------------------------------------------------dispatch
 	let dispatch = {
@@ -26,10 +12,15 @@
 
 	function Start(com, fun){
 		console.log("--Bandit/Start");
-		setTimeout(()=>{
-			log.d(`Terminating the current process with exit code 72`)
-			process.exit(72);
-		}, 4000);
+		this.Vlt.pd = this.require("probability-distributions");
+
+		
+
+
+		// setTimeout(()=>{
+		// 	log.d(`Terminating the current process with exit code 72`)
+		// 	process.exit(72);
+		// }, 4000);
 		fun(com);
 	}
 
