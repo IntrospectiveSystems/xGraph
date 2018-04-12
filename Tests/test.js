990f91d6f395bdd1f7d4ca1e2f4b029a9d84da37
Marcus Gosselin <marcus13345@live.com>
2018-04-12 15:01:09 -0400

    Testing Properly, nothing is implemented, everything fails

--- Tests/test.js
+++ Tests/test.js
@@ -11,12 +11,17 @@ try {
 	exec('npm install -g .');
 	exec('which xgraph');
 	exec('xgraph -v');
-	exec('xgraph c --CWD ValidateSystem --local ./ValidateSystem/ --core');
+
+	// move in to the test directory
+	process.chdir('Tests');
+	exec('xgraph c --CWD ValidationSystem --local ./ValidationSystem/Modules');
+
+	exec('xgraph d --CWD ValidationSystem --local ./ValidationSystem/Modules');
 
 	console.log('');
 	process.exit(0);
 } catch (e) {
-	console.error(e);
+	console.log('');
 	process.exit(1);
 }
 
