e8cd16d585d4775cc508f1a91669b52f36051aa9
Amanda <amanda.castonguay@introspectivesystems.com>
2018-04-23 11:41:20 -0400

    Updated command line parameter error.

--- src/Genesis.js
+++ src/Genesis.js
@@ -1007,7 +1007,7 @@ function genesis(__options = {}) {
 							if (param in Params)
 								s += Params[param];
 							else
-								throw 'Parameter <' + param + '> not defined';
+								throw 'Required command line parameter <' + param + '> is not defined.';
 							state = 1;
 						} else {
 							param += chr;
